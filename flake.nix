400dfaef2315f1d2327ca8d8ff784d6eae4f757b
Ben Jack <benthejack@gmail.com>
2023-08-12 20:28:50 +0100

    initial

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: 
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [ pkgs.imagemagick ];
+    };
+  };
+}
